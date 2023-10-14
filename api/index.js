9d3f4cf5d44e2cfea377d33c1724136bcce102ec
Saksham <saksham.gairola28@gmail.com>
2023-10-14 12:33:36 +0530

    updated db connection url

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
 });
 app.use('/users', userRoutes);
 
-const CONNECTION_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@registeredusers.82qivcn.mongodb.net/?retryWrites=true&w=majority`;
+const CONNECTION_URL = `${process.env.DB_CONNECTION}`;
 const PORT = process.env.PORT || 5000;
 
 mongoose
